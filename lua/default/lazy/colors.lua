188cc27d9f939340b03887251508e74d150966c3
David Vogel <david@davidvogel.xyz>
2025-06-08 20:43:45 -0600

    FIX: hotfixes for "update for Neovim v0.11", round 1
    changes in this commit include:
    - added `lazy-lock.json` to `.gitignore`
    - removed unnecessary comments from `colors.lua`
    - added helpful comments to settings in `colors.lua`
    - removed LuaRocks as a requirement in `lazy_init.lua`

--- lua/default/lazy/colors.lua
+++ lua/default/lazy/colors.lua
@@ -1,27 +1,17 @@
--- Set colorscheme using a protected call (in case it's not installed)
---local status, _ = pcall(vim.cmd, "colorscheme synthwave84")
---if not status then
---    print("Colorscheme 'synthwave84' not found! Using default colorscheme.")
---    return
---end
-
 function ColorMyPencils(color)
     color = color or "synthwave84"
     vim.cmd.colorscheme(color)
 
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-
-    -- other settings for `ColorMyPencils`
-    --vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
-    --vim.api.nvim_set_hl(0, "CursorLine", { bg = "none" })
-    --vim.api.nvim_set_hl(0, "MsgArea", { bg = "none" })
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" }) -- `Normal`is the main window
+    vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" }) -- `NormalNC` is the main window when a float appears
+    --vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" }) -- `NormalFloat` is a floating window (ex. Lazy menu, Harpoon menu)
+    --vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" }) -- `TelescopeNormal` is the main Telescope window (techinically, a floating window)
     --vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "none" })
-    --vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
     --vim.api.nvim_set_hl(0, "TelescopeSelection", { bg = "none" })
     --vim.api.nvim_set_hl(0, "TelescopeSelectionCaret", { bg = "none" })
+    --vim.api.nvim_set_hl(0, "CursorLine", { bg = "none" })
+    vim.api.nvim_set_hl(0, "MsgArea", { bg = "none" }) -- `MsgArea` is the little area between the `vim-airline` bar and the `tmux` status bar
 end
-
 
 return {
     {
