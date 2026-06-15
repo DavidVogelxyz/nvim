be192383f46ef810b807fdd1a52152e05e410b7c
David Vogel <david@davidvogel.xyz>
2026-06-14 20:39:09 -0600

    UPDATE: added `treesitter-context.lua`

---
+++ lua/default/lazy/treesitter-context.lua
@@ -0,0 +1,21 @@
+return {
+    "nvim-treesitter/nvim-treesitter-context",
+    opts = function()
+        --local tsc = require("treesitter-context")
+        --Snacks.toggle({
+        --    name = "Treesitter Context",
+        --    get = tsc.enabled,
+        --    set = function(state)
+        --        if state then
+        --            tsc.enable()
+        --        else
+        --            tsc.disable()
+        --        end
+        --    end,
+        --}):map("<leader>ut")
+        return {
+            mode = "topline", -- Line used to calculate context. Choices: 'cursor', 'topline'
+            max_lines = 3, -- How many lines the window should span. Values <= 0 mean no limit.
+        }
+    end,
+}
